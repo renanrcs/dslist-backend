7fdf9167ac6c8f28287eb13947e39164e4b2c717
renanrcs <renan.cavalcante@yahoo.com>
2023-05-09 06:12:54 -0300

    project created

---
+++ src/test/java/io/renanrcs/dslist/DslistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.renanrcs.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
